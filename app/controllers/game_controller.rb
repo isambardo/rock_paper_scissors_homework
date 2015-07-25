8f910e888c32dd2eefe5cc0fbf18064212459e6b
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-07-24 19:22:35 -0500

    game works, database not established

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,4 +6,33 @@ class GameController < ApplicationController
 
   # Your code goes below.
 
+  def play
+
+    # Rails, behind the scenes:
+    # params = {"move"=>"rock"}
+
+    @user_move = params["the_move"]
+
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @user_move == @computer_move
+      @outcome = "tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @outcome = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @outcome = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @outcome = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @outcome = "won"
+    end
+
+
+
+    render("play.html.erb")
+  end
 end
