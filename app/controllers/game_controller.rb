825b606a4d6eb09a56431f24a13da46113f3cb50
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-07-27 13:40:54 -0500

    builds faulty database

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,6 +8,8 @@ class GameController < ApplicationController
 
   def play
 
+    @all_moves = Move.all
+
     # Rails, behind the scenes:
     # params = {"move"=>"rock"}
 
@@ -16,7 +18,7 @@ class GameController < ApplicationController
     @computer_move = ["rock", "paper", "scissors"].sample
 
     if @user_move == @computer_move
-      @outcome = "tied"
+      @outcome = "tie"
     elsif @user_move == "paper" && @computer_move == "rock"
       @outcome = "won"
     elsif @user_move == "paper" && @computer_move == "scissors"
@@ -31,7 +33,23 @@ class GameController < ApplicationController
       @outcome = "won"
     end
 
-
+    m = Move.new
+    m.user_move = @user_move
+    m.computer_move = @computer_move
+    if @outcome == "won"
+      m.user_wins = 1
+      m.computer_wins = 0
+      m.tie = 0
+    elsif @outcome == "lost"
+      m.user_wins = 0
+      m.computer_wins = 1
+      m.tie = 0
+    elsif @outcome == "tie"
+      m.user_wins = 0
+      m.computer_wins = 0
+      m.tie = 1
+    end
+    m.save
 
     render("play.html.erb")
   end
